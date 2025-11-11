fdcccbf6cd97a4a3b2df804638fca2e58639d3b7
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-11-11 07:56:06 -0500

    changed the action pressed parameters to mimic the new input map

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -12,13 +12,13 @@ func _physics_process(delta):
 	var direction = Vector3.ZERO
 
 	# We check for each move input and update the direction accordingly.
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("Move_Right"):
 		direction.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("Move_Left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
+	if Input.is_action_pressed("Move_Backward"):
 		# Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_pressed("Move_Forward"):
 		direction.z -= 1
