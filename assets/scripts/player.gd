42f28496b3a28cda548ac15fa6e33bb8a78388fd
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-11-11 09:36:40 -0500

    we now have gravity being forced down on our player node.

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -5,11 +5,14 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+# Variable to store vertical velocity
+var vertical_velocity = 0
+
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
+	# Create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
-	# We check for each move input and update the direction accordingly.
+	# Check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("Move_Right"):
 		direction.x += 1
 	if Input.is_action_pressed("Move_Left"):
@@ -23,7 +26,12 @@ func _physics_process(delta):
 	direction = direction.normalized()
 
 	# Calculate the target velocity based on input direction and speed.
-	var target_velocity = direction * speed  # Set the target velocity
+	var target_velocity = direction * speed 
+
+	# Manage vertical velocity for falling
+	if(!is_on_floor()):
+		vertical_velocity -= fall_acceleration * delta  # Apply falling acceleration
+	target_velocity.y = vertical_velocity  # Set vertical velocity
 
 	# Move the character.
 	velocity = target_velocity  # Assign the target velocity to the character's velocity
